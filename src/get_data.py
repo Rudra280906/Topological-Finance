0971c13cad5ac29e6f6440c18c9ab2fb3dda3dd9
Rudra280906 <rudra280906@gmail.com>
2026-04-22 04:33:42 -0700

    Transformed data to get u and d for binomial asset pricing model

--- src/get_data.py
+++ src/get_data.py
@@ -1,9 +1,23 @@
 import yfinance as yf
 import pandas as pd
+import numpy as np
 
-tickerStrings = ['SPY', 'QQQ', 'SVIX', 'UVIX', 'FNGU', 'TQQQ']
+tickerStrings = ['SPY']
 df_list = []
+
+def transform(data):
+    returns = np.log(data['Close'] / data['Close'].shift(1))
+    volatility = np.std(returns)
+    u = np.exp(volatility)
+    d = np.exp(-volatility)
+    print(u)
+    print(d)
+
+
 for ticker in tickerStrings:
-    data = yf.download(ticker, group_by="Ticker", period='2y')
+    data = yf.download(ticker, group_by="Ticker", period='2y', auto_adjust=False)
+    data.columns = ['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']
     data['ticker'] = ticker
+    print(data)
     pd.DataFrame.to_csv(data, "Topological-Finance/data/%s.csv" % ticker)
+    transform(data)
